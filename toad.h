3b1701181f8c4ce104ca28a804941e20b2cfb48a
Jvill909 <76712060+Jvill909@users.noreply.github.com>
2021-01-14 14:46:38 -0800

    Add files via upload

---
+++ toad.h
@@ -0,0 +1,16 @@
+#ifndef Project1_toad_h
+#define Project1_toad_h
+
+#include "life.h"
+
+class Life;
+
+class Toad : public Life {
+public:
+
+	// Constructor/destructor
+	Toad(int r, int c);
+	~Toad();
+};
+
+#endif
